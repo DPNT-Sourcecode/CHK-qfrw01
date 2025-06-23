7998db877d150032139d4f8d5aeaeebc4f390f1e
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-06-23 10:57:14 +0000

    Mon Jun 23 10:57:14 UTC 2025

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -5,27 +5,27 @@ var assert = require('assert');
 const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
 
 describe('CHK challenge: calculating total checkout', function () {
-    it('should return 130, for A discount', function () {
-        assert.equal(new CheckoutSolution().checkout('AAA'), 130);
+    it('should return 0, for no items', function () {
+        assert.equal(new CheckoutSolution().checkout(''), 0);
     });
 
-    it('should return 45, for B discount', function () {
-        assert.equal(new CheckoutSolution().checkout('BB'), 45);
+    it('should return 80, for AB', function () {
+        assert.equal(new CheckoutSolution().checkout('AB'), 80);
     });
 
-    it('should return 115, which is the sum of all four items', function () {
-        assert.equal(new CheckoutSolution().checkout('ABCD'), 115);
+    it('should return 200, for 5As', function () {
+        assert.equal(new CheckoutSolution().checkout('AAAAA'), 200);
     });
 
-    it('should return -1, for non-string input', function () {
-        assert.equal(new CheckoutSolution().checkout(15), -1);
+    it('should return 330, for 8As', function () {
+        assert.equal(new CheckoutSolution().checkout('AAAAAAAA'), 330);
     });
 
-    it('should return -1, for non-item input', function () {
-        assert.equal(new CheckoutSolution().checkout('ABSDF'), -1);
+    it('should return 75, for 3Bs', function () {
+        assert.equal(new CheckoutSolution().checkout('BBB'), 75);
     });
 
-    it('should return -1, for multiple item combination input', function () {
-        assert.equal(new CheckoutSolution().checkout('ABCCAABDBA'), 310);
+    it('should return 200, for 4Es and 4Bs', function () {
+        assert.equal(new CheckoutSolution().checkout('EEEEBBBB'), 200);
     });
 });
