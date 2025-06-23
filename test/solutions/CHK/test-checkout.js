9520dde6e14c5f596b18474bd9a91bb2bb9e4c2a
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-06-23 10:23:15 +0000

    Mon Jun 23 10:23:15 UTC 2025

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -5,7 +5,23 @@ var assert = require('assert');
 const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
 
 describe('CHK challenge: calculating total checkout', function () {
-    it('should return 3, which is the sum of 1 and 2', function () {
-        assert.equal(new CheckoutSolution().checkout('AAABCDA'), 245);
+    it('should return 130, for A discount', function () {
+        assert.equal(new CheckoutSolution().checkout('AAA'), 130);
+    });
+
+    it('should return 45, for B discount', function () {
+        assert.equal(new CheckoutSolution().checkout('BB'), 45);
+    });
+
+    it('should return 115, which is the sum of all four items', function () {
+        assert.equal(new CheckoutSolution().checkout('ABCD'), 115);
+    });
+
+    it('should return -1, for non-string input', function () {
+        assert.equal(new CheckoutSolution().checkout(15), -1);
+    });
+
+    it('should return -1, for non-item input', function () {
+        assert.equal(new CheckoutSolution().checkout('ABSDF'), -1);
     });
 });
