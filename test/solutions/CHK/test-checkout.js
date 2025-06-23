707e93fca89b16a007424618fbd2022db8fe385f
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-06-23 10:20:15 +0000

    Mon Jun 23 10:20:15 UTC 2025

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -2,10 +2,10 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const SumSolution = require('../../../lib/solutions/SUM/sum_solution');
+const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
 
 describe('CHK challenge: calculating total checkout', function () {
     it('should return 3, which is the sum of 1 and 2', function () {
-        assert.equal(new SumSolution().compute(1, 2), 3);
+        assert.equal(new CheckoutSolution().checkout('AAABCDA'), 245);
     });
 });
