c894cbd048f98bc98b626f6fb77c74db4c3b94e4
sbx_user1051 <sbx_user1051@169.254.5.201>
2025-06-23 10:02:15 +0000

    Mon Jun 23 10:02:15 UTC 2025

--- test/solutions/HLO/test-hello-world.js
+++ test/solutions/HLO/test-hello-world.js
@@ -4,7 +4,7 @@ var it = mocha.it
 var assert = require('assert');
 const HelloSolution = require('../../../lib/solutions/HLO/hello_solution');
 
-describe('SUM challenge: adding two numbers', function () {
+describe('HLO challenge: Greeting our friends', function () {
     it('should return Hello, John!', function () {
         assert.equal(new HelloSolution().hello('John'), "Hello, John!");
     });
