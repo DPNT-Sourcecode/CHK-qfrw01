c1ed5444717f12f571a91ad0926633f8a1df770e
sbx_user1051 <sbx_user1051@169.254.84.57>
2025-06-23 09:38:07 +0000

    Mon Jun 23 09:38:07 UTC 2025

--- test/solutions/SUM/sum_solution.js
+++
@@ -1,3 +0,0 @@
-export function SumSolution(a, b) {
-    return a + b;
-}
--- test/solutions/SUM/test-sum-solution.js
+++ test/solutions/SUM/test-sum-solution.js
@@ -4,7 +4,6 @@ var it = mocha.it
 var assert = require('assert');
 const SumSolution = require('../../../lib/solutions/SUM/sum_solution');
 
-console.log(SumSolution(1, 2))
 describe('SUM challenge: adding two numbers', function () {
 	it('should return 3, which is the sum of 1 and 2', function () {
 		assert.equal(new SumSolution().compute(1, 2), 3);
